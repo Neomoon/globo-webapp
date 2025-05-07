ffb525d4266ca5be98cb0358cee1dded2f97880a
Eliezer Luna <neomoon@gmail.com>
2025-05-07 15:39:53 -0500

    Switch to using Ansible for nginx install

--- variables.tf
+++ variables.tf
@@ -37,6 +37,11 @@ variable "api_key" {
   description = "(Required) API key for web app to talk to SaaS platform."
 }
 
+variable "playbook_repository" {
+  type        = string
+  description = "(Required) Uri of Ansible playbook"
+}
+
 variable "tfe_organization" {
   type        = string
   description = "(Optional) Terraform Cloud Organization name"
