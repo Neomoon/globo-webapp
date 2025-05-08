7d84d112a623b9c3352c9450b9eb1879481a513b
Eliezer Luna <neomoon@gmail.com>
2025-05-08 12:31:20 -0500

    Switch to using secrets manager

--- resources.tf
+++ resources.tf
@@ -34,11 +34,11 @@ resource "aws_iam_instance_profile" "main" {
 
 
 resource "aws_instance" "main" {
-  count         = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
-  ami           = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
+  count                = length(data.tfe_outputs.networking.nonsensitive_values.public_subnets)
+  ami                  = nonsensitive(data.aws_ssm_parameter.amzn2_linux.value)
   iam_instance_profile = aws_iam_instance_profile.main.name
-  instance_type = var.instance_type
-  subnet_id     = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
+  instance_type        = var.instance_type
+  subnet_id            = data.tfe_outputs.networking.nonsensitive_values.public_subnets[count.index]
   vpc_security_group_ids = [
     aws_security_group.webapp_http_inbound_sg.id,
     aws_security_group.webapp_ssh_inbound_sg.id,
@@ -56,8 +56,8 @@ resource "aws_instance" "main" {
   user_data = templatefile("./templates/userdata.sh", {
     playbook_repository = var.playbook_repository
     secret_id           = var.api_key_secret_id
-    host_list_ssm_name  = var.host_list_ssm_name
-    site_name_ssm_name  = var.site_name_ssm_name
+    host_list_ssm_name  = local.host_list_ssm_name
+    site_name_ssm_name  = local.site_name_ssm_name
   })
 
 }
